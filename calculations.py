4f45c9121e45e80c2b497ae1a8e8eed5ba79b8fd
Jessica Roland <jroland015004@gmail.com>
2020-01-07 12:06:20 -0800

    Update to formatting of dates; calculate annual inflation and add to dataframe

--- calculations.py
+++ calculations.py
@@ -5,20 +5,22 @@ shill_data = pd.read_csv('data/shiller_monthly_data.csv', index_col=False)
 
 refined_data = shill_data[['Date', 'Consumer Price Index CPI','Long Interest Rate GS10','Real Price', 'Real Dividend']]
 
-refined_data['Date'] = refined_data['Date'].map('{:.2f}'.format)
+refined_data['Date'] = pd.to_numeric(refined_data['Date']).map('{:.2f}'.format)
+
 dec_cpi = refined_data['Consumer Price Index CPI'][[x.endswith('12') for x in refined_data['Date']]].reset_index()
-print(dec_cpi)
+#print(dec_cpi)
 jan_cpi = refined_data['Consumer Price Index CPI'][[x.endswith('01') for x in refined_data['Date']]].reset_index()
 jan_cpi = jan_cpi[0:-1]       #removes Jan 2019 since Dec 2019 data not available
 #print(jan_cpi)
 annual_inflation = (dec_cpi-jan_cpi)/jan_cpi
 #print(annual_inflation)
-refined_data['Annual Inflation'] = 'NA'
-refined_data['Annual Inflation'][[x.endswith('12') for x in refined_data['Date']]] = annual_inflation['Consumer Price Index CPI']
-print(refined_data['Annual Inflation'][0:20])
 
-#refined_data.info()
-#create column for inflation rate based on CPI
+refined_data['Annual Inflation'] = 'NA'
+for i in range(len(dec_cpi)):
+    refined_data['Annual Inflation'][dec_cpi['index'][i]] = annual_inflation['Consumer Price Index CPI'][i]
+#refined_data[0:50]
+
+#stretch goal -- create column for monthly inflation rate based on CPI
 
 
 portfolio = 1000000                  #initial portfolio amount at retirement, using $1M as starting example
