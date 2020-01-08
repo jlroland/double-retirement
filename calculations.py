ec81ee2c4ffd71373b841b58b81384eb19cb8de3
Jessica Roland <jroland015004@gmail.com>
2020-01-07 16:52:02 -0800

    Plot of portfolio progress for all 30-yr and 60-yr periods

--- calculations.py
+++ calculations.py
@@ -22,25 +22,70 @@ dec_data['Annual Gain/Loss'] = ((dec_data['Real Price']-jan_data['Real Price'])/
 #dec_data
 #stretch goal -- create column in refined_data for monthly gain/loss
 
+#start with traditional 30-yr for comparison
+def calculate_30(start, wr, ax):
+    port_values = []
+    init_portfolio = 1000000
+    port_values.append(init_portfolio)
+    withdrawal = init_portfolio*wr
+    year1_balance = init_portfolio-withdrawal
+    equity_portion = 0.5*year1_balance 
+    bond_portion = 0.5*year1_balance
+    
+    for i in range(start, start+29):
+        num_shares = equity_portion/dec_data['Real Price'][i]
+        portfolio = (equity_portion*(1+dec_data['Annual Gain/Loss'][i]) + num_shares*dec_data['Real Dividend'][i]) + (bond_portion*(1+dec_data['Long Interest Rate GS10'][i]))
+        withdrawal += withdrawal*dec_data['Annual Inflation'][i]
+        portfolio -= withdrawal
+        port_values.append(portfolio)
+        equity_portion = 0.5*portfolio
+        bond_portion = 0.5*portfolio
+    ax.plot(range(start-1,start+29), port_values)
+    return portfolio
 
-
-init_portfolio = 1000000                  #initial portfolio amount at retirement, using $1M as starting example    
-swr = 0.04                            #safe withdrawal rate, using 4% as starting example
-year1_balance = init_portfolio*(1-swr)          #withdraw funds for first year of retirement on Jan. 1
-equity_portion = 0.5*year1_balance       #50% of portfolio invested in S&P index
-bond_portion = 0.5*year1_balance         #50% of portfolio invested in 10-yr Treasury bonds
+def calculate_60(start, wr, ax):
+    port_values = []
+    init_portfolio = 1000000
+    port_values.append(init_portfolio)
+    withdrawal = init_portfolio*wr
+    year1_balance = init_portfolio-withdrawal 
+    equity_portion = 0.5*year1_balance 
+    bond_portion = 0.5*year1_balance
+    
+    for i in range(start, start+59):
+        num_shares = equity_portion/dec_data['Real Price'][i]
+        portfolio = (equity_portion*(1+dec_data['Annual Gain/Loss'][i]) + num_shares*dec_data['Real Dividend'][i]) + (bond_portion*(1+dec_data['Long Interest Rate GS10'][i]))
+        withdrawal += withdrawal*dec_data['Annual Inflation'][i]
+        portfolio -= withdrawal
+        port_values.append(portfolio)
+        equity_portion = 0.5*portfolio
+        bond_portion = 0.5*portfolio
+    ax.plot(range(start-1,start+59), port_values)
+    return portfolio
 
 fig, ax = plt.subplots()
-for i in range(59):
-    num_shares = equity_portion/dec_data['Real Price'][i]
-    portfolio = (equity_portion*(1+dec_data['Annual Gain/Loss'][i]) + num_shares*dec_data['Real Dividend'][i]) + (bond_portion*(1+dec_data['Long Interest Rate GS10'][i]))
-    portfolio = portfolio*(1-(swr*(1+dec_data['Annual Inflation'][i])))
-    equity_portion = 0.5*portfolio
-    bond_portion = 0.5*portfolio
-    ax.scatter(i, portfolio)
+success30 = 0
+failure30 = 0
 
 
+for i in range(1,120):
+    result30 = calculate_30(i, 0.04, ax)
+    if result30 > 0:
+        success30 += 1
+    elif result30 <= 0:
+        failure30 +=1
+print(success30, failure30)
 
+fig, ax = plt.subplots()
+success60 = 0
+failure60 = 0
+for i in range(1,90):
+    result = calculate_60(i, 0.04, ax)
+    if result > 0:
+        success60 += 1
+    elif result <= 0:
+        failure60 +=1
+print(success60, failure60)
 
 
 
