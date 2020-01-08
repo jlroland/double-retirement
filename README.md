720d4d12d55219d1028bd921e52d205f3e9cd8bd
Jessica Roland <jroland015004@gmail.com>
2020-01-08 13:17:19 -0800

    Calculate probabilities; create plot and add to README

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Traditionally, retirement planning has focused on building a portfolio that will sustain a 30-year retirement (e.g. retire at age 65 with enough funds to last until age 95). Historical analysis has shown that withdrawing 4% of a portfolio in each year of retirement has a 95% chance of successfully funding a 30-year retirement. This assumes a 50/50 asset allocation of equities (S&P index) and bonds (10-year Treasury yield). It also assumes that the withdrawal amount is adjusted for inflation each year.
 
 As people live longer and try to retire earlier, it would be helpful to examine retirement planning over a longer time horizon. A growing number of people live past the age of 100, and a movement focused on retiring early has gained momentum in recent years. Under such circumstances, what is the probability of a retirement portfolio lasting 60 years instead of 30 years at a 4% withdrawal rate? Would a lower withdrawal rate increase the probability of success significantly?
+
+Assumptions:
+1. Brokerage fees and taxes are not considered (i.e. they should be paid from withdrawn funds).
+2. Inflation is based on changes in CPI.
+3. Asset allocation of 50/50 is maintained year to year.
+4. A single withdrawal is made on Jan. 1 each year to cover expenses for the year ahead. Yearly gains/losses on the remaining balance are calculated on Dec. 31 that year.
+
+
+!(prob_range_comparison.png)
--- calculations.py
+++ calculations.py
@@ -1,6 +1,7 @@
 %matplotlib inline
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
@@ -22,71 +23,80 @@ dec_data['Annual Gain/Loss'] = ((dec_data['Real Price']-jan_data['Real Price'])/
 #dec_data
 #stretch goal -- create column in refined_data for monthly gain/loss
 
+#quarterly compounding of bond interest
+def calculate_interest(principal, rate):
+    return principal*((1+rate/4)**4)
+
 #start with traditional 30-yr for comparison
 def calculate_30(start, wr, ax):
     port_values = []
     init_portfolio = 1000000
     port_values.append(init_portfolio)
-    withdrawal = init_portfolio*wr
-    year1_balance = init_portfolio-withdrawal
+    withdrawal_amount = init_portfolio*wr
+    year1_balance = init_portfolio-withdrawal_amount
     equity_portion = 0.5*year1_balance 
     bond_portion = 0.5*year1_balance
     
     for i in range(start, start+29):
         num_shares = equity_portion/dec_data['Real Price'][i]
-        portfolio = (equity_portion*(1+dec_data['Annual Gain/Loss'][i]) + num_shares*dec_data['Real Dividend'][i]) + (bond_portion*(1+dec_data['Long Interest Rate GS10'][i]))
-        withdrawal += withdrawal*dec_data['Annual Inflation'][i]
-        portfolio -= withdrawal
+        portfolio = (equity_portion*(1+dec_data['Annual Gain/Loss'][i]) + num_shares*dec_data['Real Dividend'][i]) + calculate_interest(bond_portion, dec_data['Long Interest Rate GS10'][i])
+        withdrawal_amount += withdrawal_amount*dec_data['Annual Inflation'][i]
+        portfolio -= withdrawal_amount
         port_values.append(portfolio)
         equity_portion = 0.5*portfolio
         bond_portion = 0.5*portfolio
-    ax.plot(range(start-1,start+29), port_values)
+    ax.plot(range(1,31), port_values)
     return portfolio
 
 def calculate_60(start, wr, ax):
     port_values = []
     init_portfolio = 1000000
     port_values.append(init_portfolio)
-    withdrawal = init_portfolio*wr
-    year1_balance = init_portfolio-withdrawal 
+    withdrawal_amount = init_portfolio*wr
+    year1_balance = init_portfolio-withdrawal_amount 
     equity_portion = 0.5*year1_balance 
     bond_portion = 0.5*year1_balance
     
     for i in range(start, start+59):
         num_shares = equity_portion/dec_data['Real Price'][i]
-        portfolio = (equity_portion*(1+dec_data['Annual Gain/Loss'][i]) + num_shares*dec_data['Real Dividend'][i]) + (bond_portion*(1+dec_data['Long Interest Rate GS10'][i]))
-        withdrawal += withdrawal*dec_data['Annual Inflation'][i]
-        portfolio -= withdrawal
+        portfolio = (equity_portion*(1+dec_data['Annual Gain/Loss'][i]) + num_shares*dec_data['Real Dividend'][i]) + calculate_interest(bond_portion, dec_data['Long Interest Rate GS10'][i])
+        withdrawal_amount += withdrawal_amount*dec_data['Annual Inflation'][i]
+        portfolio -= withdrawal_amount
         port_values.append(portfolio)
         equity_portion = 0.5*portfolio
         bond_portion = 0.5*portfolio
-    ax.plot(range(start-1,start+59), port_values)
+    ax.plot(range(1,61), port_values)
     return portfolio
 
-fig, ax = plt.subplots()
-success30 = 0
-failure30 = 0
+def prob_success_30(wr):
+    success = 0
+    for i in range(0,120):
+        result = calculate_30(1000000, i, wr, ax)
+        if result > 0:
+            success += 1
+    return success/120
+
+def prob_success_60(wr):
+    success = 0
+    for i in range(0,90):
+        result = calculate_60(1000000, i, wr, ax)
+        if result > 0:
+            success += 1
+    return success/90
+
+wr_list = np.linspace(0, 0.1, 21)
+prob_30 = []
+prob_60 = []
+for i in range(len(wr_list)):
+    prob_30.append(prob_success_30(wr_list[i]))
+    prob_60.append(prob_success_60(wr_list[i]))
 
 
-for i in range(1,120):
-    result30 = calculate_30(i, 0.04, ax)
-    if result30 > 0:
-        success30 += 1
-    elif result30 <= 0:
-        failure30 +=1
-print(success30, failure30)
-
-fig, ax = plt.subplots()
-success60 = 0
-failure60 = 0
-for i in range(1,90):
-    result = calculate_60(i, 0.04, ax)
-    if result > 0:
-        success60 += 1
-    elif result <= 0:
-        failure60 +=1
-print(success60, failure60)
-
-
-
-
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=wr_list, y=prob_30, name='30-yr retirement'))
+fig.add_trace(go.Scatter(x=wr_list, y=prob_60, name='60-yr retirement'))
+fig.update_layout(title='Probability of Portfolio Success Based on Withdrawal Rate',
+                   xaxis_title='Initial Withdrawal Rate',
+                   yaxis_title='Probability of Success')
+fig.show()
+#fig.write_image('prob_range_comparison.png')
